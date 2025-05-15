e56f781feb761a95034cb6168d9a659f099ca266
Adrian Rodel Bangalando <bangalandoam@students.national-u.edu.ph>
2025-05-15 12:56:17 +0800

    Create morse_utils.py

---
+++ morse_utils.py
@@ -0,0 +1,18 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': '/'
+}
+
+INVERSE_MORSE_CODE_DICT = {v: k for k, v in MORSE_CODE_DICT.items()}
+
+def text_to_morse(text):
+    return ' '.join(MORSE_CODE_DICT.get(char.upper(), '') for char in text)
+
+def morse_to_text(morse):
+    return ''.join(INVERSE_MORSE_CODE_DICT.get(code, '') for code in morse.split())
