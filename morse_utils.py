0e3c2717419f3bffb40f71632d2132cd5ed25439
shinkairu <keithdelcarmen1@gmail.com>
2025-05-21 22:13:49 +0800

    Update morse_utils.py
    added morse table dictionary

--- morse_utils.py
+++ morse_utils.py
@@ -8,6 +8,26 @@ morse_dict = {
     '8': '---..', '9': '----.'
 }
 
+morse_table = """
+### Morse Code Reference Table
+
+| Character | Morse Code | Character | Morse Code | Character | Morse Code |
+|-----------|------------|-----------|------------|-----------|------------|
+| A         | .-         | N         | -.         | 0         | -----      |
+| B         | -...       | O         | ---        | 1         | .----      |
+| C         | -.-.       | P         | .--.       | 2         | ..---      |
+| D         | -..        | Q         | --.-       | 3         | ...--      |
+| E         | .          | R         | .-.        | 4         | ....-      |
+| F         | ..-.       | S         | ...        | 5         | .....      |
+| G         | --.        | T         | -          | 6         | -....      |
+| H         | ....       | U         | ..-        | 7         | --...      |
+| I         | ..         | V         | ...-       | 8         | ---..      |
+| J         | .---       | W         | .--        | 9         | ----.      |
+| K         | -.-        | X         | -..-       |           |            |
+| L         | .-..       | Y         | -.--       |           |            |
+| M         | --         | Z         | --..       |           |            |
+"""
+
 inverse_dict = {v: k for k, v in morse_dict.items()}
 
 def text_to_morse(text):
